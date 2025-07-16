82479f1b337a45ac29f96ef9b4f9dfd05cfc9bd6
Waleed Bin Shehab <waleedbinshihab@gmail.com>
2025-07-16 19:27:10 +0300

    feat: undo refactoring

---
+++ lib/features/issue_production_orders/presentation/view/screens/all_issue_production_orders_screen.dart
@@ -0,0 +1,13 @@
+
+import 'package:manufacturing_facilities_management/core/src/manufacturing_facilities_management_export.dart';
+
+import '../widgets/details_issue_production_orders.dart';
+
+class AllIssueProductionOrdersScreen extends StatelessWidget {
+  const AllIssueProductionOrdersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const DetailsIssueProductionOrders();
+  }
+}
---
+++ lib/features/issue_production_orders/src_issue_production_orders.dart
@@ -0,0 +1,8 @@
+export './data/repos/issue_production_orders_repo.dart';
+export './data/models/issue_production_orders_model.dart';
+export './data/datasources/issue_production_orders_remote_ds.dart';
+export './domain/usecases/issue_production_orders_usecase.dart';
+export './domain/entities/issue_production_orders_ent.dart';
+export './domain/contracts/issue_production_orders_cont.dart';
+export './presentation/view/screens/issue_production_orders_screen.dart';
+export './presentation/view/screens/all_issue_production_orders_screen.dart';
