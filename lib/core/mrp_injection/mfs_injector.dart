b4c1d25287cc6558ed6c977aa89519e9d6ab1923
AhmedALyousafi <ahmedalyousafi@gmail.com>
2025-07-20 09:14:00 +0300

    first commit

--- lib/core/mrp_injection/mfs_injector.dart
+++ lib/core/mrp_injection/mfs_injector.dart
@@ -1,4 +1,6 @@
 import 'package:manufacturing_facilities_management/core/featuers/manufacturing_facilities_management/issue_production_orders/presentation/controllers/issue_production_orders_cubit.dart';
+import 'package:manufacturing_facilities_management/core/featuers/manufacturing_facilities_management/material_order_release/presentation/controllers/material_order_release_cubit.dart';
+import 'package:manufacturing_facilities_management/core/featuers/manufacturing_facilities_management/material_return/presentation/controllers/material_return_cubit.dart';
 import 'package:manufacturing_facilities_management/core/src/manufacturing_facilities_management_export.dart';
 
 class MfsInitGetIt{
@@ -7,6 +9,14 @@ class MfsInitGetIt{
 
     getIt.registerFactory<IssueProductionOrdersCubit>(
             () => IssueProductionOrdersCubit());
+
+                 /// ✨ <<<--------------------- Material Order Release  ----------------------->>>
+  getIt.registerFactory<MaterialOrderReleaseCubit>(
+      () => MaterialOrderReleaseCubit());
+
+        /// ✨ <<<--------------------- Material Return  ----------------------->>>
+  getIt.registerFactory<MaterialReturnCubit>(
+      () => MaterialReturnCubit());
   }
 }
 
